9a06cdb003790980c790404152ac9535d059c20f
Matthew Cania <matthewgab24@gmail.com>
2025-05-28 21:15:30 +0800

    Finish Backend Login

--- Backend/controllers/accountsController.js
+++ Backend/controllers/accountsController.js
@@ -1,19 +1,80 @@
 import { UserAccountsModel, UserProfilesModel } from "../models/index.js"
 
-async function registerAccount () {
+async function registerAccount (req, res) {
 
 	const {email, password, firstname, middlename, lastname, address, contacts} = req.body
 
 	try {
-		const accountRes = await UserAccounts
+		const accountRes = await UserAccountsModel.create({
+			email,
+			password
+		})
+		if (!accountRes) {
+			throw new Error("Account Creation Error")
+		}
+		const profileRes = await UserProfilesModel.create({
+			firstname, 
+			middlename,
+			lastname,
+			address,
+			contacts
+		})
+
+		const accountId = accountRes.accountId
+
+		if (!profileRes) {
+			await UserAccountsModel.destroy({
+				where: {accountId}
+			})
+			throw new Error('Profile Creation Error')
+		}
 	} catch (error) {
-		
+		return res.status(500).json({error: error.message})
 	}
-
-
 }
 
-function loginAccount () {
+async function loginAccount (req, res) {
+
+	const {email, password} = req.body;
+
+	try {
+		const emailRes = await UserAccountsModel.findOne({where: {email}})
+		
+		if (!emailRes){
+			throw new Error('Email Not Found!')
+		}
+
+		const accountPassword = emailRes.password
+
+		const isMatch = bcrypt.compare(password, accountPassword)
+		if (!isMatch) {
+			throw new Error('Incorrect Password')
+		}
+
+		const accountId = emailRes.accountId
+		const accountEmail = emailRes.email
+
+		const profileRes = await UserProfilesModel.findOne({where: {accountId}})
+
+		if (!profileRes){
+			throw new Error('Account Profile Not Found!')
+		}
+
+		const accountName = `${profileRes.firsName} ${profileRes.middleName} ${profileRes.lastName}`
+
+		const token = {
+			accountId,
+			accountEmail,
+			accountName
+		}
+
+		res.status(200).json({message: 'Log In Successful', token: token})
+
+
+
+	} catch (error) {
+		return res.status(500).json({error: error.message})
+	}
 
 }
 
